f2d8d15b1887960c472cb102653068d482808407
Denys209818 <deniskravcuk63@gmail.com>
2024-12-04 19:18:44 +0200

    make build

---
+++ index.7ebb7361.js
@@ -0,0 +1,2 @@
+document.getElementById("email-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation(),document.getElementsByClassName("main__nav-email")[0].value=""});
+//# sourceMappingURL=index.7ebb7361.js.map
